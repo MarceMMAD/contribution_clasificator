e1b6bfb87265efb49ca4d4f0b077c4ecbacb77ab
Bartlomiej Balcerzak <b.balcerzak@pjwstk.edu.pl>
2018-05-12 15:08:37 +0200

    I have a message from Michelle, from the resistance. The python file contains an important function

---
+++ mp.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+
+def the_spanish_inquisition(somebody):
+    if somebody == 'nobody':
+        print (somebody + " expects the spanish inquistion!!!")
+    else:
+        print( "The comfy chair?!")
+
+print (the_spanish_inquisition("not_nobody"))
